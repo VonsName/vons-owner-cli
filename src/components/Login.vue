90417c020b40480aa51640d4af5a7c57666b6776
fjl <fengjianglin6817@dingtalk.com>
2018-10-23 19:27:00 +0800

    Initial commit

---
+++ src/components/Login.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+      <p>{{message}}</p>
+      <br/>
+      <h1>von-owner</h1>
+      <input type="text" v-model="username"/>
+      <input type="password" v-model="password"/>
+      <input type="button" @click="login" value="登录"/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Login",
+        data(){
+          return {
+            message:'von-owner',
+            username:undefined,
+            password:undefined
+          }
+        },
+      methods:{
+          login:function () {
+            // console.log(this.username);
+            // console.log(this.password);
+            this.$http.get('/login/userLogin',{
+                params:{
+                  username:this.username,
+                  password:this.password
+                }
+            }).then(function (res) {
+              console.log(res);
+            }).catch(function (res) {
+              console.log(res);
+            })
+          }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
